69225e93cdcbf062bbfa55be906c35cb7afe0683
DeViktor <contato.regedit@gmail.com>
2022-06-30 17:42:44 -0400

    finished account maneger

--- src/services/AccountManagerServices.ts
+++ src/services/AccountManagerServices.ts
@@ -1,7 +1,33 @@
 import prismaClient from "../prisma"
 
 class AccountManagerServices {
-    async create(idENVIA: String, idRECEBE: String, value) {
+    async create(idENVIA: string, idRECEBE: string, value: number) {
+        const checkIfHave = await prismaClient.account.findFirst({
+            where: { userId: idENVIA }
+        })
+        if ( checkIfHave.mount < value ) return 
+
+        const checkRecive = await.prismaClient.account.findFirst({
+            where:{ id: idRECEBE}
+        })
+
+        let aux = checkIfHave.mount - value;
+
+        const upEnvia = await prismaClient.account.update({
+            where:{
+                userId: idENVIA
+            },
+            data:{ mount: aux }
+        })
+
+        aux = checkRecive.mount + value;
+        const upRecebe = await prismaClient.account.update({
+            where:{
+                userId: idRECEBE
+            },
+            data:{ mount: aux }
+        })
+
         // const to_check = await prismaClient.account.findFirst({
         //     where: {
         //         userId: idENVIA
@@ -23,7 +49,7 @@ class AccountManagerServices {
         // return "Sem Saldo Disponivel"
     }
     // async create(request: Request, response: Response) {
-    //     const check = await prismaClient.account.findFirst({
+//     const check = await prismaClient.account.find!First({
     //         where: {
     //         }
     //     })
